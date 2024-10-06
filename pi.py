ae9df7b30636576cc310246c1e81ddcaa1ad99e2
s1f10220252 <107392875+s1f10220252@users.noreply.github.com>
2024-10-06 15:12:23 +0900

    Add pi.py template

---
+++ pi.py
@@ -0,0 +1,4 @@
+def get_pi_digits():
+    text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
+
+    # ここに実装を書く
